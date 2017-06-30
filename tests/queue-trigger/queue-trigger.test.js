18bd765a254b6dae33e5e765d123ea9749c0d956
James Sturtevant <jsturtevant@gmail.com>
2017-06-30 16:08:44 -0400

    added directions on how to use w/ tape

--- tests/queue-trigger/queue-trigger.test.js
+++ tests/queue-trigger/queue-trigger.test.js
@@ -4,7 +4,6 @@ var func = require('../../src/index.js');
 test('Queue trigger', function(group){
     var queueFunc = func('queue-func', {dirname: __dirname}); 
     
-
     group.test('promise should return output on binding', function (t){
         t.plan(1);
 
