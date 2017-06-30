b1d3d1d7539fdeb7573e2e40a11f0c7ae18b0b6e
James Sturtevant <jsturtevant@gmail.com>
2017-06-30 10:44:13 -0400

    added first tests

--- test/test.js
+++ test/test.js
@@ -1,9 +1,27 @@
+var test = require("tape");
 var func = require('../src/index.js');
 
-var queueFunc = func('queue', {dirname: __dirname}); 
-var invocation = queueFunc.invoke({trigger: {'hello':'world'}});
+test('Queue trigger', function(group){
+    var queueFunc = func('queue', {dirname: __dirname}); 
+    
 
-invocation.then(function(data) {
-    console.log('done');
-    console.log(JSON.stringify(data, null, ' ' ));
-})
+    group.test('promise should return output on binding', function (t){
+        t.plan(1);
+
+        queueFunc.invoke({trigger: {'hello':'world'}}).then(function(context) {
+            t.equal("foobar", context.bindings.output);
+        }).catch(err => {
+            t.fail("Failed because of: " + err)
+        });
+    });
+
+    group.test('callback should return output on binding', function (t){
+        t.plan(1);
+
+        queueFunc.invoke({trigger: {'hello':'world'}}, function(context){
+            t.equal("foobar", context.bindings.output);
+        });
+    });
+
+    group.end();
+});
